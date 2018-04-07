f0ed59657708ac1ca5c52591fcd90ad90cf15101
Isaac Wong <isaac_wch@outlook.com>
2018-04-08 00:39:02 +0800

    added key file

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express'
-//import { GraphQLSchema, GraphQLObjectType, GraphQLString } from 'graphql'
 import {makeExecutableSchema} from 'graphql-tools'
 
 import mongoose from 'mongoose'
 import types from './schema'
 import Note from './model'
 import resolvers from './resolver'
+import keys from './config/keys'
 
 const app = express()
 
-mongoose.connect('mongodb://admin:Password@ds137019.mlab.com:37019/notepad-db')
+mongoose.connect('mongodb://' + keys.userID+':'+keys.Password+'@ds137019.mlab.com:37019/notepad-db')
 
 
 const schema = makeExecutableSchema({
@@ -19,7 +19,7 @@ const schema = makeExecutableSchema({
     resolvers,
   })
 
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema, context: {Note}})) 
-app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' })) // enable GraphiQL
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema, context: {Note}})) //The context is an object that’s accessible in every single resolver as the third argument.
+app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' })) 
 
 app.listen(4000)
